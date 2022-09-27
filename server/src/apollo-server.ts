253a61df51c124d0292bf50f140c1c77f2aad66d
Ruben Nunez <ruben_abdias@live.com>
2022-09-27 11:39:15 -0500

    Setup Apollo Server

---
+++ server/src/apollo-server.ts
@@ -0,0 +1,46 @@
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ExpressContext, ApolloServer, Config } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import { Server } from 'http';
+import { resolvers } from '@generated/type-graphql';
+import { PrismaClient } from '@prisma/client';
+
+async function startApolloServer(
+  serverOptions: {
+    httpServer: Server;
+    app: express.Application;
+    db: PrismaClient;
+  },
+  apolloOptions: {
+    schemaWithResolvers: Exclude<Config<ExpressContext>['schema'], undefined>;
+  }
+): Promise<ApolloServer<ExpressContext>> {
+  const { httpServer, app, db } = serverOptions;
+  const { schemaWithResolvers } = apolloOptions;
+  const server = new ApolloServer({
+    schema: schemaWithResolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: () => {
+      return { db };
+    },
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  return server;
+}
+
+export async function createApolloServer(
+  db: PrismaClient,
+  httpServer: Server,
+  app: express.Application
+): Promise<ApolloServer<ExpressContext>> {
+  // Add resolvers to the schema
+  const schemaWithResolvers = await buildSchema({ resolvers });
+
+  const apolloServer = await startApolloServer(
+    { httpServer, app, db },
+    { schemaWithResolvers }
+  );
+  return apolloServer;
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,26 @@
+import 'reflect-metadata';
+import { PrismaClient } from '@prisma/client';
+import { createApolloServer } from './apollo-server';
 import * as chalk from 'chalk';
-import * as express from 'express';
-// import { createServer } from 'http';
 import { PORT } from './constants';
+import * as express from 'express';
+import { createServer } from 'http';
+
+const prisma = new PrismaClient();
 
 const app = express();
 async function main() {
-  // const httpServer = createServer(app);
+  const httpServer = createServer(app);
+  const apolloServer = await createApolloServer(prisma, httpServer, app);
 
   await new Promise<void>((resolve) => {
     app.listen(PORT, () => {
       console.log(
         [
           chalk.bgMagentaBright.black.bold(' GraphQL API listening on   '),
-          chalk.bgWhite.black(`\thttp://localhost:${PORT}\t`),
+          chalk.bgWhite.black(
+            `\thttp://localhost:${PORT}${apolloServer.graphqlPath}\t`
+          ),
         ].join(' ')
       );
       resolve();
